24991c08d046cfec032a476475e27c695bb40e70
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2025-06-01 14:45:27 +0530

    refactor: added findByUsername method to UserRepository for user lookup by username

--- src/main/java/com/dipanshushukla/realtimechatappmessageservice/repository/UserRepository.java
+++ src/main/java/com/dipanshushukla/realtimechatappmessageservice/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.dipanshushukla.realtimechatappmessageservice.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.dipanshushukla.realtimechatappmessageservice.entity.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+  Optional<User> findByUsername(String username);
 
 }
