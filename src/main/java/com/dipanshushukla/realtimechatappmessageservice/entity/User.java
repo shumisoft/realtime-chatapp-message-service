095d8e98898a2340c02881742c420dc4daf08778
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2025-12-01 19:46:45 +0530

    test-fix: quote reserved keyword "user" in H2 table name

--- src/main/java/com/dipanshushukla/realtimechatappmessageservice/entity/User.java
+++ src/main/java/com/dipanshushukla/realtimechatappmessageservice/entity/User.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "\"user\"")
 public class User {
 
     @Id
